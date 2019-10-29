1cb176b7f8041fbeb673d196f0d343e347be709c
2accessany <2accessany@gmail.com>
2019-10-29 18:16:35 +0530

    Added test for lc_1165

---
+++ src/tests/Lc1165_SingleRowKeyboardTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import code.premium.Lc1165_SingleRowKeyboard;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Lc1165_SingleRowKeyboardTest {
+
+    @Test
+    public void calculateTime() {
+        Lc1165_SingleRowKeyboard solution = new Lc1165_SingleRowKeyboard();
+        assertEquals(solution.calculateTime("pqrstuvwxyzabcdefghijklmno", "leetcode"), 73);
+    }
+}
