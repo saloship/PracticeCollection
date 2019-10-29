dfc07333856cedc0a9bd4ccbb538575b3aa18932
2accessany <2accessany@gmail.com>
2019-10-29 18:08:01 +0530

    Added Definition for LC760

--- src/code/Lc760_FindAnagramMapping.java
+++ src/code/Lc760_FindAnagramMapping.java
@@ -5,9 +5,27 @@ import java.util.Map;
 
 public class Lc760_FindAnagramMapping extends BaseSolution{
 
+
+  /*
+  * Given two lists Aand B, and B is an anagram of A. B is an anagram of A means B is made by randomizing the order of the elements in A.
+
+We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
+
+These lists A and B may contain duplicates. If there are multiple answers, output any of them.
+
+For example, given
+
+A = [12, 28, 46, 32, 50]
+B = [50, 12, 32, 46, 28]
+We should return
+[1, 4, 3, 2, 0]*/
+
+
     @Override
     public void solveIt() {
-
+        int[] A = {12, 28, 46, 32, 50};
+        int[] B = {50, 12, 32, 46, 28};
+        anagramMappings(A, B);
     }
 
     public int[] anagramMappings(int[] A, int[] B) {
