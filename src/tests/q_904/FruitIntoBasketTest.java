6fd89315a57fb7e08ba723cfc734f60ad69b80af
Ashok Soni <ashoksoni22@gmail.com>
2020-10-11 19:46:21 +0530

    Added test for q_904

---
+++ src/tests/q_904/FruitIntoBasketTest.java
@@ -0,0 +1,25 @@
+package tests.q_904;
+
+import leetcode.normal.Q_904.FruitIntoBasket;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FruitIntoBasketTest {
+
+    @Test
+    public void mostCommonWord() {
+        FruitIntoBasket solution = new FruitIntoBasket();
+        int[] arr1 = {1,2,1};
+        assertEquals(solution.totalFruit(arr1), 3);
+
+        int[] arr2 = {0,1,2,2};
+        assertEquals(solution.totalFruit(arr2), 3);
+
+        int[] arr3 = {1,2,3,2,2};
+        assertEquals(solution.totalFruit(arr3), 4);
+
+        int[] arr4 = {3,3,3,1,2,1,1,2,3,3,4};
+        assertEquals(solution.totalFruit(arr4), 5);
+    }
+}
