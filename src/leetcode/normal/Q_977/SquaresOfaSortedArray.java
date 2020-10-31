576e59fdc9db1ef1cf6e36a23e4c050ea84eafa2
2AccessAny <2accessany@gmail.com>
2020-10-31 18:19:16 +0530

    added q_977 definition

---
+++ src/leetcode/normal/Q_977/SquaresOfaSortedArray.java
@@ -0,0 +1,30 @@
+package leetcode.normal.Q_977;
+
+import leetcode.BaseSolution;
+
+public class SquaresOfaSortedArray extends BaseSolution {
+    /*
+    https://leetcode.com/problems/squares-of-a-sorted-array/
+
+    Given an array of integers A sorted in non-decreasing order,
+    return an array of the squares of each number, also in sorted non-decreasing order.
+
+     Example 1:
+     Input: [-4,-1,0,3,10]
+     Output: [0,1,9,16,100]
+
+    Example 2:
+    Input: [-7,-3,2,3,11]
+    Output: [4,9,9,49,121]
+     */
+
+    @Override
+    public void solveIt() {
+
+    }
+
+    public int[] sortedSquares(int[] A) {
+       return null;
+    }
+
+}
