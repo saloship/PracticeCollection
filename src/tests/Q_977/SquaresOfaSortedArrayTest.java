434bb50eeb0cb97c31d4604fbd8f19fb5bc934ab
2AccessAny <2accessany@gmail.com>
2020-10-31 18:42:50 +0530

    Added squaresOfSortedArray Test class

---
+++ src/tests/Q_977/SquaresOfaSortedArrayTest.java
@@ -0,0 +1,13 @@
+package tests.Q_977;
+
+import leetcode.normal.Q_760.FindAnagramMapping;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SquaresOfaSortedArrayTest {
+    @Test
+    public void squaresOfaSortedArray() {
+
+    }
+}
