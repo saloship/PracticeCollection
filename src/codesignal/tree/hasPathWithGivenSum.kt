0b1c7f9828a9ec42d5688f6d00f7137c0a37220f
Navratan <navratansoni70@gmail.com>
2022-10-05 22:17:49 +0530

    Create hasPathWithGivenSum.kt

---
+++ src/codesignal/tree/hasPathWithGivenSum.kt
@@ -0,0 +1,38 @@
+package codesignal.tree
+
+fun main() {
+    val tree = hasPathWithGivenSum.Tree(5, hasPathWithGivenSum.Tree(7, null, null), null)
+    println(hasPathWithGivenSum().solution(tree, 12))
+}
+
+class hasPathWithGivenSum {
+
+    // Given a binary tree t and an integer s, determine whether there is a root to leaf path in t such that the sum of vertex values equals s.
+
+    data class Tree<T>(
+        var value: T,
+        var left: Tree<T>? = null,
+        var right: Tree<T>? = null
+    )
+
+    fun solution(t: Tree<Int>?, s: Int): Boolean {
+        if (t == null)
+            return false
+        return isPathWithGivenSum(t, s, 0)
+    }
+
+    fun isPathWithGivenSum(node: Tree<Int>?, pathSum: Int, currentSum: Int): Boolean {
+        var newSum = currentSum + node!!.value
+        if (node.left == null && node.right == null) {
+            return newSum == pathSum
+        }
+        return node.left?.let { isPathWithGivenSum(it, pathSum, newSum) } ?: false || node.right?.let {
+            isPathWithGivenSum(
+                it,
+                pathSum,
+                newSum
+            )
+        }?: false
+    }
+
+}
